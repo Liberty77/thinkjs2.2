eb40c8af69996a569125f3259039c3f25345be1b
Liberty77 <1037091775@qq.com>
2018-06-01 06:36:35 +0800

    01

--- src/common/config/config.js
+++ src/common/config/config.js
@@ -6,4 +6,6 @@
 //将默认模块名改为 blog
 export default {
     //key: value
+    default_module: 'blog',
+    deny_module_list: ['test']
 };
--- src/home/controller/article.js
+++ src/home/controller/article.js
@@ -3,24 +3,14 @@
 import Base from './base.js';
 
 export default class extends Base {
+
     /**
      * index action
      * @return {Promise} []
      */
     indexAction() {
+
         //auto render template file index_index.html
         return this.display();
-    }
-
-    init(http) {
-        super.init(http);
-    }
-
-    /**
-     * 前置方法  方法会在具体的 Action 调用之前自动调
-     * @return {Promise} []
-     */
-    __before() {
-
     }
 }
